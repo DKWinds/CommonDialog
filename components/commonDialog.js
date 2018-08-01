a3df73e51486066a324aed520586fe5ca9fee694
DKWinds <41983242+DKWinds@users.noreply.github.com>
2018-08-01 14:11:19 +0800

    Update commonDialog.js
    delete the repeat style code, and definition of canonical variables.

--- components/commonDialog.js
+++ components/commonDialog.js
@@ -119,15 +119,15 @@ export default class CommonDialog extends Component{
   */  
 	show(options) { 
 	  if(options){
-		clickScreen=options.clickScreen==undefined?true:options.clickScreen;
-		animationType=options.animationType==undefined?'fade':options.animationType;
-		title=options.title==undefined?'提示':options.title;
-		thide=options.thide==undefined?false:options.thide;
-		headStyle=options.headStyle==undefined?'':options.headStyle;
-		messText=options.messText==undefined?'':options.messText;
-		innersWidth=options.innersWidth==undefined?null:options.innersWidth;
-	    innersHeight=options.innersHeight==undefined?null:options.innersHeight;
-		buttons=options.buttons==undefined?null:options.buttons;
+		let clickScreen=options.clickScreen==undefined?true:options.clickScreen;
+		let animationType=options.animationType==undefined?'fade':options.animationType;
+		let title=options.title==undefined?'提示':options.title;
+		let thide=options.thide==undefined?false:options.thide;
+		let headStyle=options.headStyle==undefined?'':options.headStyle;
+		let messText=options.messText==undefined?'':options.messText;
+		let innersWidth=options.innersWidth==undefined?null:options.innersWidth;
+	        let innersHeight=options.innersHeight==undefined?null:options.innersHeight;
+		let buttons=options.buttons==undefined?null:options.buttons;
 		if(!this.state.modalVisible){  
 		  this.setState({
 			  title:title,
@@ -250,11 +250,6 @@ const styles = StyleSheet.create({
 		borderRadius:3,
 		textAlignVertical:'center',
 	},
-	boxCenter:{
-		justifyContent:'center',
-		alignItems:'center',
-	},
-	
 	Jcenter:{
 		justifyContent:'center',
 	},
